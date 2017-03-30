c03176d921ddd90da1658ce91bfcf6058974af73
liviabrandaao <liviaabrandaao@gmail.com>
2017-03-29 21:39:30 -0300

    Alteração service

--- js/service/medicoService.js
+++ js/service/medicoService.js
@@ -3,7 +3,7 @@ angulaAppModulo.factory("MedicoService", function($http) {
     var urlBase = "http://localhost:8080/GestansApplication";
     
     var _cadastrarMedico = function(medico){
-		return $http.post(urlBase + "/medico/inserir", medico)
+		return $http.post(urlBase + "/medico/cadastro", medico)
 	};
 
     return {
